fafd69fe2b87a73c47169ead6c39b43951c23fbd
Yograj Nimgaonkar <120590665+Sage2910@users.noreply.github.com>
2023-01-11 22:03:47 +0530

    Update Day1.js

--- Day1.js
+++ Day1.js
@@ -1,18 +1,18 @@
 //program to check if a number is prime in javascript
 function check_prime(n){
   if (n===1){
-    return false;
+    return "It is not Prime";
   }
   else if(n === 2){
-    return true;
+    return "It is Prime";
   }
   else{
     for(var x = 2; x < n; x++){
       if(n % x === 0){
-        return false;
+        return "It is not Prime";
       }
     }
-    return true;  
+    return "It is Prime";  
   }
 }
 
