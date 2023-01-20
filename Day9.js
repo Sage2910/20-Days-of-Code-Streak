450912702cf253e5022cbf0c340d1c92c1e3628d
Yograj Nimgaonkar <120590665+Sage2910@users.noreply.github.com>
2023-01-20 23:47:37 +0530

    Create Day9.js

---
+++ Day9.js
@@ -0,0 +1,8 @@
+// program to convert first letter of a string to uppercase
+function capitalizeFirstLetter(str) {
+    const capitalized = str.charAt(0).toUpperCase() + str.slice(1);
+    return capitalized;
+}
+const string = prompt('Enter a string: ');
+const result = capitalizeFirstLetter(string);
+console.log(result)
