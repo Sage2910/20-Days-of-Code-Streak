0691d861571568013d04e30e3839741db6afa958
Yograj Nimgaonkar <120590665+Sage2910@users.noreply.github.com>
2023-01-23 23:37:24 +0530

    Create Day12.js

---
+++ Day12.js
@@ -0,0 +1,8 @@
+// program to check if a string contains a substring
+const str = prompt('Enter a string:');
+const checkString = prompt('Enter a string that you want to check:');
+if(str.includes(checkString)) {
+    console.log(`The string contains ${checkString}`);
+} else {
+    console.log(`The string does not contain ${checkString}`);
+}
